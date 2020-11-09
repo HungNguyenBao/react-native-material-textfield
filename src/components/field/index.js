cb61d387b059fe0f81365fa2c838a42ede67729b
BaoHungNguyen <baohung2412@gmail.com>
2020-11-09 12:14:37 +0700

    Update index.js

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -139,7 +139,7 @@ export default class TextField extends PureComponent {
     y1: 0,
   };
 
-  static getDerivedStateFromProps({ error }, state) {
+  static getDerivedStateFromProps({ error, value }, state) {
     /* Keep last received error in state */
     if (error && error !== state.error) {
       return { error };
